7b4f108fa9e3f8d62747cd09d5d6acef3257d8a6
Tianren Qin <qtren2015@hotmail.com>
2022-07-11 16:36:57 -0400

    parse values from resultset

--- readme.md
+++ readme.md
@@ -1,8 +1,12 @@
 # Query from Big Query in Rust
 
-I am a rust noob. This is just me, learning about Rust and and trying to use it (potentially) for ETL purposes. Here I managed to query results from Big Query and get a ResultSet back. I have no idea how to turn this ResultSet into anything else (Too lazy of me to dig that deep), but ideally if I want to use this for my ETL, then I should at least be able to send the result data to some SQL database or to csv file.
+I am a rust noob. This is just me, learning about Rust and and trying to use it (potentially) for ETL purposes. Here I managed to query results from Big Query and get a ResultSet back. Then I am able to parse through the data and get the max close price.
 
-Anyway, using Rust for I/O tasks is always hard and fun.
+This process is very painful (moving your cursor, iterating, no auto type inference...)
+
+Next step is to look into possibilities to improve it and combine it with other tools to actually move data from BQ to some kind of SQL database.
+
+
 
 # Project Setup and Output
 ![project setup](/pics/project_setup.PNG)
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ async fn main(){
 async fn get_data_from_bq(){
     // The environment var returns the path to the key json file
     let env_path = env::var("GOOGLE_APPLICATION_CREDENTIALS").unwrap();
-
     let project_id = "plucky-handler-325914".to_string();
     let dataset_id = "TeslaStonks".to_string();
     let table_id = "TeslaStonksDaily".to_string();
@@ -25,7 +24,7 @@ async fn get_data_from_bq(){
     let client = gcp_bigquery_client::Client::from_service_account_key_file(&env_path).await;
 
     // Query
-    let rs = client
+    let mut rs = client
         .job()
         .query(&project_id,
             QueryRequest {
@@ -39,6 +38,52 @@ async fn get_data_from_bq(){
         )
         .await.unwrap();
 
-    println!("Number of rows: {}", rs.row_count())
+    // if result is empty, then stop or send warning.
+    
+    
+    println!("Number of rows: {}", rs.row_count());
+    println!("Column names are: {:?}", rs.column_names());
+
+    let mut close_prices:Vec<f64> = Vec::with_capacity(rs.row_count());
+    let mut dates:Vec<String> = Vec::with_capacity(rs.row_count());
+    let close_idx = *rs.column_index("Open").unwrap();
+    let date_idx = *rs.column_index("Date").unwrap();
+    let mut counter = 0;
+    while rs.next_row() {
+        
+        let mut price:f64 = -1.0;
+        match rs.get_f64(close_idx) {
+            Ok(v) => {
+                match v {
+                    Some(v) => price = v,
+                    _ => println!("Null value detected in returned dataset at row: {}", counter)
+                }
+            },
+            Err(v) => {
+                println!("Error encountered when executing BQ Query. Details: {}", v);
+            }
+        }
+
+        counter += 1;
+        close_prices.push(price);
+        // Too lazy to use match to deal with errors...
+        dates.push(rs.get_string(date_idx).expect("Erron when reading date").unwrap());
+    };
+
+    // Find max price in existing history. close_prices should not be empty.
+    let (max_index, max_price) = 
+        close_prices.iter().enumerate().fold(
+            (usize::MIN, f64::MIN),
+            |(i_a, a), (i_b, &b)| {
+                if b > a {
+                    (i_b, b)
+                } else {
+                    (i_a, a)
+                }
+            }
+        );
+
+    println!("The max price of Tesla in this dataset is {}", max_price);
+    println!("Which happened on date {}", dates[max_index]);
 
 }
